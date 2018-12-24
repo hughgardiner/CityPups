e35c373afbb4e94b632a619b46025aa0d9d3c6e2
Hugh Gardiner <gardinerhugh6@gmail.com>
2018-12-24 08:23:33 -0500

    space question page initial work

--- app/components/SizeQuestionPage/SizeQuestionPage.js
+++ app/components/SizeQuestionPage/SizeQuestionPage.js
@@ -4,32 +4,28 @@ import tinyDogIcon from '../../../assets/images/tinyDogIcon.png';
 import smallDogIcon from '../../../assets/images/smallDogIcon.png';
 import mediumDogIcon from '../../../assets/images/mediumDogIcon.png';
 import largeDogIcon from '../../../assets/images/largeDogIcon.png';
-import { QuestionPage } from '../QuestionPage/QuestionPage';
+import QuestionPage from '../QuestionPage/QuestionPage';
 
 const sizePageButtons = [
   {
     buttonText: 'Tiny',
     iconSource: tinyDogIcon,
-    iconStyle: [styles.dogIconStyle, styles.tinyDogIconSize],
-    multiSelect: true
+    iconStyle: [styles.dogIconStyle, styles.tinyDogIconSize]
   },
   {
     buttonText: 'Small',
     iconSource: smallDogIcon,
-    iconStyle: [styles.dogIconStyle, styles.smallDogIconSize],
-    multiSelect: true
+    iconStyle: [styles.dogIconStyle, styles.smallDogIconSize]
   },
   {
     buttonText: 'Medium',
     iconSource: mediumDogIcon,
-    iconStyle: [styles.dogIconStyle, styles.mediumDogIconSize],
-    multiSelect: true
+    iconStyle: [styles.dogIconStyle, styles.mediumDogIconSize]
   },
   {
     buttonText: 'Large',
     iconSource: largeDogIcon,
-    iconStyle: [styles.dogIconStyle, styles.largeDogIconSize],
-    multiSelect: true
+    iconStyle: [styles.dogIconStyle, styles.largeDogIconSize]
   }
 ]
 
@@ -40,7 +36,8 @@ export default class SizeQuestionPage extends React.Component {
       navigation: this.props.navigation,
       answerButtons: sizePageButtons,
       previousPage: 'HomePage',
-      nextPage: 'LivingQuestionPage'
+      nextPage: 'LivingQuestionPage',
+      multiSelect: true
     }
     return (
       <QuestionPage {...sizePageProps} />
---
+++ app/components/SpaceQuestionPage/SpaceQuestionPage.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './styles';
+import warningIcon from '../../../assets/images/warningIcon.png';
+import parkIcon from '../../../assets/images/parkIcon.png';
+import plantIcon from '../../../assets/images/plantIcon.png';
+import fenceIcon from '../../../assets/images/fenceIcon.png';
+import QuestionPage from '../QuestionPage/QuestionPage';
+
+const spacePageButtons = [
+    {
+      buttonText: 'No outdoor space',
+      iconStyle: [styles.spaceIconStyle],
+      iconSource: warningIcon
+    },
+    {
+      buttonText: 'Dog park nearby',
+      iconStyle: [styles.spaceIconStyle],
+      iconSource: parkIcon
+    },
+    {
+      buttonText: 'Small yard',
+      iconStyle: [styles.spaceIconStyle],
+      iconSource: plantIcon
+    },
+    {
+      buttonText: 'Good sized yard',
+      iconStyle: [styles.spaceIconStyle],
+      iconSource: fenceIcon
+    }
+]
+
+export default class SpaceQuestionPage extends React.Component {
+  render() {
+    const spacePageProps = {
+      questionText: "How much space\nwill your dog have\nto play?",
+      navigation: this.props.navigation,
+      answerButtons: spacePageButtons,
+      previousPage: 'LivingQuestionPage',
+      nextPage: 'RoommateQuestionPage',
+      multiSelect: true
+    }
+    return (
+      <QuestionPage {...spacePageProps} />
+    );
+  }
+}
